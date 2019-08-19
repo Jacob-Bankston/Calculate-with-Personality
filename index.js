a358d56791aeac0c6f550489428261dfc94b00be
Jacob Bankston <jacob.bankston.smile@gmail.com>
2019-08-19 00:12:01 -0500

    added aws and s3 links for audio files

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const express = require("express")
 const mustacheExpress = require("mustache-express")
-const app = express()
-const PORT = process.env.PORT || 8080
 const path = require("path")
 const models = require("./models")
+const aws = require('aws-sdk')
+const app = express()
+const PORT = process.env.PORT || 8080
 
 app.use(express.json())
 app.use(express.urlencoded())
 app.use(express.static("static"))
 
 const VIEWS_PATH = path.join(__dirname, "/views")
+const S3_BUCKET = process.env.S3_BUCKET
 
 app.engine("mustache", mustacheExpress(VIEWS_PATH + "/partials", ".mustache"))
 app.set("views", VIEWS_PATH)
