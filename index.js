0a54421b7d21708e470dd721d8021b103572e8e7
Taliaa <taliaa10@ymail.com>
2019-08-28 00:49:29 -0500

    mobilized site, footer, back button

--- index.js
+++ index.js
@@ -1,55 +1,53 @@
-const express = require("express")
-const mustacheExpress = require("mustache-express")
-const path = require("path")
-const models = require("./models")
-const aws = require('aws-sdk')
-aws.config.update({region: 'us-east-2'})
-const app = express()
-const PORT = process.env.PORT || 8080
+const express = require("express");
+const mustacheExpress = require("mustache-express");
+const path = require("path");
+const models = require("./models");
+const aws = require("aws-sdk");
+aws.config.update({ region: "us-east-2" });
+const app = express();
+const PORT = process.env.PORT || 8080;
 
-app.use(express.json())
-app.use(express.urlencoded())
-app.use(express.static("static"))
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.static("static"));
 
-const VIEWS_PATH = path.join(__dirname, "/views")
-const S3_BUCKET = process.env.S3_BUCKET
+const VIEWS_PATH = path.join(__dirname, "/views");
+const S3_BUCKET = process.env.S3_BUCKET;
 
-app.engine("mustache", mustacheExpress(VIEWS_PATH + "/partials", ".mustache"))
-app.set("views", VIEWS_PATH)
-app.set("view engine", "mustache")
+app.engine("mustache", mustacheExpress(VIEWS_PATH + "/partials", ".mustache"));
+app.set("views", VIEWS_PATH);
+app.set("view engine", "mustache");
 
 app.get("/", (req, res) => {
   models.Personality.findAll()
     .then(personality => {
-      // console.log(personality);
-      res.render("personality", { personality: personality })
+      res.render("personality", { personality: personality });
     })
     .catch(error => {
-      console.log(error)
+      console.log(error);
     });
 });
 
 app.get("/personality/:name", (req, res) => {
-  let personalityName = req.params.name
+  let personalityName = req.params.name;
   models.Personality.findOne({
     where: {
       name: personalityName
     }
   })
     .then(chosenPersonality => {
-      console.log(chosenPersonality)
-      res.render('index', { personality: chosenPersonality })
+      res.render("index", { personality: chosenPersonality });
     })
     .catch(error => {
       models.Personality.findAll().then(personality => {
         res.render("personality", {
           personality: personality,
           message: "Unable to find personality!"
-        })
-      })
-    })
-})
+        });
+      });
+    });
+});
 
 app.listen(PORT, () => {
-  console.log("Server is running...")
-})
+  console.log("Server is running...");
+});
